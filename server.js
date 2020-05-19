636f359010e4c284a9edd2be93c9cb3eba1b5cc3
Brandon Lou <14984696+brandonlou@users.noreply.github.com>
2020-05-19 14:49:37 -0700

    Add web scrape off foodbanks.net based on zipcode

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const http = require('http');
 const router = require('./router');
+const axios = require('axios');
+const $ = require('cheerio');
 
 const PORT = process.env.PORT || 9797;
 
@@ -10,3 +12,25 @@ const server = http.createServer(app);
 app.use(router);
 
 server.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
+
+const zipcode = "90095";
+const url = "https://www.foodbanks.net/search.php?q=" + zipcode;
+axios.get(url)
+    .then(response => {
+        getData(response.data);
+    })
+    .catch(error => {
+        console.log(error);
+    });
+
+let getData = html => {
+
+    $('table tbody tr', html).each((i, elem) => {
+
+        const name = $('span[itemprop=name]', elem).text();
+        const address = $('div[itemprop=address]', elem).text();
+        console.log(name);
+        console.log("\t" + address);
+
+    });
+}
