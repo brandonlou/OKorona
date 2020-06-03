e2370934b373d3fb85cc1d08d7a644b73a9b35de
Brandon Lou <14984696+brandonlou@users.noreply.github.com>
2020-06-02 18:24:51 -0700

    Move convert address to coords to a common module

---
+++ common.js
@@ -0,0 +1,51 @@
+const fetch = require("node-fetch");
+
+const common = {};
+
+common.convertAddressToCoords = async (address) => {
+
+    // Sleep for 2 seconds before continuing to prevent massive requests to OSM.
+    await new Promise(r => setTimeout(r, 2000));
+  
+    // No address specified.
+    if(!address) {
+      return null;
+    }
+  
+    // Convert whitespace to %20 for and remove #[num]
+    address = await address.replace(/\s+/g, "%20");
+    address = await address.replace(/#.*\s/, "%20");
+  
+    const res = await fetch("https://nominatim.openstreetmap.org/search?q=" + address + "&format=json");
+  
+    if(!res.ok) {
+        console.log(res.statusText);
+        return null;
+    }
+  
+    let data;
+    try { // Try to parse JSON response.
+      data = await res.json();
+    } catch(err) {
+      console.log(err);
+      return null;
+    }
+  
+    const location = data[0]; // Get first search result.
+  
+    // Location exists!
+    if(location) {
+        return {
+          lat: location.lat,
+          lon: location.lon
+        }
+  
+    // Location doesn't exist :(
+    } else {
+        console.log(address + " not found on OSM!");
+        return null;
+    }
+  
+};
+
+module.exports = common;
